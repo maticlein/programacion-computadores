d6588a6d263d616b7d6691b25cd0c1527d481c45
Matthias Clein <m.cleinespinoza@gmail.com>
2021-10-13 18:51:16 -0300

    primeros ejercicios lab7

---
+++ lab-7/actividad4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main(){
+    int numero = 0;
+    printf("Ingrese un número: ");
+    scanf("%i", &numero); // 7
+
+    for(int i = 2 ; i <= numero ; i++){
+        // printf("BUSCAR SI EL %i TIENE 2 DIVISORES\n", i);
+        int divisores = 0;
+        for(int j = 2 ; j < i ; j++){
+            // printf("REVISANDO: %i\n", j);
+            int resto = i%j;
+            if(resto == 0){
+                // printf("El %i es divisor de %i\n", j, i);
+                divisores++;
+            } else {
+                // printf("El %i no es divisor de %i\n", j, i);
+            }
+        }
+        if(divisores == 0){
+            printf("EL %i ES PRIMO!\n", i);
+        } else {
+            printf("EL %i ES NO PRIMO!\n", i);
+        }
+    }
+    // 2  3  4  5    OK
+    // ?  ?  ?  ?  
+    //  2
+    //  3       1      2            3       
+    //  4       1    2   3          4
+    //  5       1    2  3  4        5
+    //  n       1    2      n-1     n
+    // ?: PRIMO
+
+    return 0;
+}
---
+++ lab-7/actividad6.c
@@ -0,0 +1,10 @@
+a = 0
+b = 1
+
+numero = 3
+
+for(int i = 0 ; i < numero){
+a + b = 1   ---   a = 1     b =1
+a + b = 2   ---   a = 1     b = 2
+a + b = 3   ---   a = 2     b = 3
+}
