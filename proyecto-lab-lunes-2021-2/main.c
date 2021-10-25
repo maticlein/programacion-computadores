5a6dcdd403387ec485047ac0684b3cee168f9e9f
Matthias Clein <m.cleinespinoza@gmail.com>
2021-10-25 19:09:43 -0300

    avance proyecto lab lunes

--- proyecto-lab-lunes-2021-2/main.c
+++ proyecto-lab-lunes-2021-2/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
     int opcion = 0;
@@ -21,22 +22,62 @@ int main(){
     printf("6.- Salir\n");
     
     // Ingresar la opción
-    printf("Por favor seleccione una opción:");
+    printf("Por favor seleccione una opción: ");
     scanf("%i", &opcion);
+
+    // Validar el dato
+    while((opcion < 1) || (opcion > 6)){
+        system("clear");
+        printf("Opción inválida por favor ingrese nuevamente: ");
+        scanf("%i", &opcion);
+    }
 
     // Ejecutar una acción dependiendo de la opción elegida 
     switch(opcion){
-        case 1: printf("Operaciones básicas\n");
+        case 1: system("clear");
+                printf("Operaciones básicas\n");
+                printf("1.- Suma\n");
+                printf("2.- Resta\n");
+                printf("3.- Multiplicación\n");
+                printf("4.- División\n");
+                printf("Por favor seleccione una opción: ");
+                scanf("%i", &opcion);
+                // Validar
+                while((opcion < 1) || (opcion > 4)){
+                        system("clear");
+                        printf("Opción inválida por favor ingrese nuevamente: ");
+                        scanf("%i", &opcion);
+                }
+                switch(opcion){
+                        case 1: system("clear");
+                                printf("Suma\n");
+                                break;
+                        case 2: system("clear");
+                                printf("Resta\n");
+                                break;
+                        case 3: system("clear");
+                                printf("Multiplicación\n");
+                                break;
+                        case 4: system("clear");
+                                printf("División\n");
+                                break;
+                        default: printf("La opción ingresada no es válida!\n");
+                }
                 break;
-        case 2: printf("Cálculo de pendiente de una recta\n");
+        case 2: system("clear");
+                printf("Cálculo de pendiente de una recta\n");
                 break;
-        case 3: printf("Distancia entre dos puntos\n");
+        case 3: system("clear");
+                printf("Distancia entre dos puntos\n");
                 break;
-        case 4: printf("Solucionador de ecuaciones cuadráticas\n");
+        case 4: system("clear");
+                printf("Solucionador de ecuaciones cuadráticas\n");
                 break;
-        case 5: printf("Triángulos\n");
+        case 5: system("clear");
+                printf("Triángulos\n");
                 break;
-        case 6: printf("Salir\n");
+        case 6: system("clear");
+                printf("Salir\n");
                 break;
         default: printf("La opción ingresada no es válida!\n");
     }
