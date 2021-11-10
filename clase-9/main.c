f8cda20688f0fa2152a56a972810d69a4d192e7a
Matthias Clein <m.cleinespinoza@gmail.com>
2021-11-10 17:01:36 -0300

    Clase 9 - Buenas prácticas en programación

---
+++ clase-9/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+    
+    /*
+    Inicio del código
+    que va a permitir realizar cálculos 
+    con números ingresados por el usuario
+    a través de la línea de comandos
+    */
+    
+    // Declaración de variables
+    int a;
+    int b; 
+    int c; 
+    int temperaturaFinal; // Variable para almacenar el resultado final
+
+    // Mensaje de saludo
+    printf("Hola!\n");
+    printf("Ingrese un número: ");
+    scanf("%i", &a);
+    //b = a + temperaturaFinal;
+    b = a + b * c;
+    
+    //printf("resultado");
+    printf("resultado");
+
+    return 0;
+}
